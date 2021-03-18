c1b65d544feb0f3289d9e00b14f97e173ff3e85a
Aishwarya Shelar <35885223+aishwaryashelar3097@users.noreply.github.com>
2021-03-18 15:20:32 +0530

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -7,7 +7,7 @@ public class PasswordValidator {
 
     // digit + lowercase char + uppercase char + punctuation + symbol
     private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()â€“[{}]:;',?/*~$^+=<>]).{8,20}$";
 
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
@@ -18,3 +18,22 @@ public class PasswordValidator {
 
 }
 }
+package com.mkyong.regex.username;
+
+
+
+public class UsernameValidator {
+
+    // simple regex
+    //private static final String USERNAME_PATTERN = "^[a-z0-9\\._-]{5,20}$";
+
+    // strict regex
+    private static final String USERNAME_PATTERN =
+            "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$";
+
+    private static final Pattern pattern = Pattern.compile(USERNAME_PATTERN);
+
+    public static boolean isValid(final String username) {
+        Matcher matcher = pattern.matcher(username);
+        return matcher.matches();
+    }
